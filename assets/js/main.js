eb5ca0822dc61bf57a67ca613112c609b51e64c4
Anniballi-Andrea <andreanniballi@gmail.com>
2025-10-07 14:39:17 +0200

    add base logic

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+/*Consegna:
+Scrivi un programma che stampi i numeri da 1 a 100,
+ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Come faccio a sapere se un numero è divisibile per?
+Abbiamo visto qualcosa di particolare che possiamo usare?*/
+
+/*Tools:
+-let e const
+-ciclo for
+-if/else if/ else
+-% x == 0
+ */
+//creare una variabile per il risultato
+
+//creare un ciclo for per i numeri che vanno da 1 a 100
+
+//filtrare i numeri multipli di 3 e di 5 
+
+// filtrare i numeri multipli di 3
+
+// filtrare i numeri multipli di 5
+
+// stampare i numeri rimanenti
